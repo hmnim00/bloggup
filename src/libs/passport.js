3efa3fec25fbd1d48adea7108f22d4d5bed4d91c
Manuel Flores <m.flores0704@outlook.com>
2021-07-12 09:10:05 -0500

    idioma

--- src/libs/passport.js
+++ src/libs/passport.js
@@ -1,59 +1,77 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
-const User = require('../models/user');
+const User = require("../models/user");
 
 // Función de autenticación de usuarios
-passport.use('local.signin', new LocalStrategy({
-  usernameField: 'username',
-  passReqToCallback: true
-}, async (req, username, password, done) => {
+passport.use(
+  "local.signin",
+  new LocalStrategy(
+    {
+      usernameField: "username",
+      passReqToCallback: true,
+    },
+    async (req, username, password, done) => {
+      try {
+        const user = await User.findOne({ username });
 
-  try {
-    const user = await User.findOne({ username });
-
-    if (!user) {
-      return done(null, false, req.flash('signinMessage', 'Username or password are invalid'));
-    } else {
-      const match = await user.matchPassword(password);
-      if (match) {
-        return done(null, user);
-      } else {
-        return done(null, false, req.flash('signinMessage', 'Username or password are invalid'));
+        if (!user) {
+          return done(
+            null,
+            false,
+            req.flash("signinMessage", "Usuario o contraseña inválidos")
+          );
+        } else {
+          const match = await user.matchPassword(password);
+          if (match) {
+            return done(null, user);
+          } else {
+            return done(
+              null,
+              false,
+              req.flash("signinMessage", "Usuario o contraseña inválidos")
+            );
+          }
+        }
+      } catch (error) {
+        console.log("Error => ", error);
       }
     }
-  } catch (error) {
-    console.log('Error => ', error);
-  }
-}));
+  )
+);
 
 // funcion de registro de usuarios
-passport.use('local.signup', new LocalStrategy({
-  usernameField: 'username',
-  passReqToCallback: true
-}, async (req, username, password, done) => {
-  const { fullname } = req.body;
+passport.use(
+  "local.signup",
+  new LocalStrategy(
+    {
+      usernameField: "username",
+      passReqToCallback: true,
+    },
+    async (req, username, password, done) => {
+      const { fullname } = req.body;
 
-  const user = new User({
-    fullname,
-    username,
-    password
-  });
+      const user = new User({
+        fullname,
+        username,
+        password,
+      });
 
-  const checkUsername = await User.findOne({ username });
-  if (checkUsername) {
-    return done(null, false);
-  }
-  try {
-    user.password = await user.encryptPassword(password);
-    await user.save();
-    console.log(user);
-    return done(null, user);
-  } catch (error) {
-    console.log(error);
-  }
-
-}));
+      const checkUsername = await User.findOne({ username });
+      if (checkUsername) {
+        return done(null, false);
+      }
+      try {
+        user.password = await user.encryptPassword(password);
+        await user.save();
+        console.log(user);
+        return done(null, user);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  )
+);
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
--- src/libs/validate.js
+++ src/libs/validate.js
@@ -1,48 +1,72 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
-const { check } = require('express-validator');
+const { check } = require("express-validator");
 const validations = {};
 
 // validación de registro de usuarios
 validations.validateSignup = [
-  check('fullname').trim().notEmpty().withMessage('Full name is required')
-    .matches(/^[a-zA-Z]{4,}(?: [a-zA-Z]+){0,2}$/).withMessage('Only characters with space are allowed'),
-  check('username').trim().notEmpty().withMessage('Username is required')
-    .matches(/^[a-zA-Z]*$/).not().matches(/^$|\s+/).withMessage('White space not allowed')
-    .custom(value => {
+  check("fullname")
+    .trim()
+    .notEmpty()
+    .withMessage("Full name is required")
+    .matches(/^[a-zA-Z]{4,}(?: [a-zA-Z]+){0,2}$/)
+    .withMessage("Solo caracteres con espacio"),
+  check("username")
+    .trim()
+    .notEmpty()
+    .withMessage("Este campo es requerido")
+    .matches(/^[a-zA-Z]*$/)
+    .not()
+    .matches(/^$|\s+/)
+    .withMessage("No se permiten espacios")
+    .custom((value) => {
       return User.findOne({ username: value }).then((username) => {
         if (username) {
-          throw new Error('Username is already taken');
+          throw new Error("El nombre de usuario no está disponible");
         }
-      })
+      });
     }),
-  check('password').trim().notEmpty().withMessage('Password is required')
-    .isLength({ min: 5 }).withMessage('Password must be minimum 5 length').matches(/^[a-zA-Z0-9]*$/).not().matches(/^$|\s+/).withMessage('White space not allowed'),
-  check('confirm').custom((value, { req }) => {
+  check("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Este campo es requerido")
+    .isLength({ min: 5 })
+    .withMessage("La contraseña debe contener al menos 5 caracteres")
+    .matches(/^[a-zA-Z0-9]*$/)
+    .not()
+    .matches(/^$|\s+/)
+    .withMessage("No se permiten espacios"),
+  check("confirm").custom((value, { req }) => {
     if (value !== req.body.password) {
-      throw new Error('Password confirmation does not match password');
+      throw new Error("Las contraseñas deben coincidir");
     }
     return true;
-  })
-]
+  }),
+];
 
 // validación de autenticación
 validations.validateSignin = [
-  check('username').trim().notEmpty().withMessage('Username is required')
-    .matches(/^[a-zA-Z]*$/).not().matches(/^$|\s+/).withMessage('White space not allowed'),
-  check('password').trim().notEmpty().withMessage('Password is required')
-]
+  check("username")
+    .trim()
+    .notEmpty()
+    .withMessage("Este campo es requerido")
+    .matches(/^[a-zA-Z]*$/)
+    .not()
+    .matches(/^$|\s+/)
+    .withMessage("No se permiten espacios"),
+  check("password").trim().notEmpty().withMessage("Este campo es requerido"),
+];
 
 // validación de creación de artículo
 validations.validateEntryForm = [
-  check('title').trim().notEmpty().withMessage('Title is required'),
-  check('description').trim().notEmpty().withMessage('Description is required'),
-  check('markdown').trim().notEmpty().withMessage('Markdown content is required')
-]
+  check("title").trim().notEmpty().withMessage("Este campo es requerido"),
+  check("description").trim().notEmpty().withMessage("Este campo es requerido"),
+  check("markdown").trim().notEmpty().withMessage("Este campo es requerido"),
+];
 
 // validación de comentario
 validations.validateComment = [
-  check('comment').trim().notEmpty().withMessage('Cannot send an empty message'),
-]
+  check("comment").trim().notEmpty().withMessage("Este campo es requerido"),
+];
 
 module.exports = validations;
